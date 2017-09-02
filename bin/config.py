84e59b9eabafcfc0169d16d9bd8593ecd996577f
SNH48 Live <snh48live@gmail.com>
2017-09-02 14:09:05 +0800

    Add helper bin/shift-configs
    For shifting download configs when the expected live_id's change (due to
    inserted events or missing VODs).

--- bin/config.py
+++ bin/config.py
@@ -1,9 +1,21 @@
+import re
 import sys
 
 import arrow
+import attrdict
 import yaml
 
-from common import THUMBNAILS_DIR, logger
+from common import THUMBNAILS_DIR, VIDEO_CONFIGS_DIR, logger
+
+
+CONFIG_FILE_PATTERN = re.compile(
+    r'^(?P<date>\d{8})'
+    r'-(?P<live_id>\d+)-'  # 0 for absence of live.snh48.com entry
+    r'(?P<stage>.*)'  # for one-off special performances, this is just
+                      # the title or abbreviated title
+    r'(-(?P<perfnum>\d{2}))?'  # only for performances within a regular stage
+    r'\.yml$'
+)
 
 
 # Attributes:
@@ -51,3 +63,30 @@ def load_vod_config(config_file):
     conf.playlists = conf_dict.get('playlists')
 
     return conf
+
+
+# Returns a list of tuples (pathlib.Path, attrdict.AttrDict) where the former
+# is the path to the config file, and the latter is an AttrDict with the
+# following attributes, all str's: date, live_id, stage, and perfnum
+# (optional).
+def list_configs(include_past=False, glob_pattern=None):
+    if glob_pattern is None:
+        glob_pattern = '**/*.yml' if include_past else '*.yml'
+
+    conf_list = []
+    for f in VIDEO_CONFIGS_DIR.glob(glob_pattern):
+        m = CONFIG_FILE_PATTERN.match(f.name)
+        if m:
+            conf_list.append((f, attrdict.AttrDict(m.groupdict())))
+        else:
+            logger.warning(f"malformed filename '{f.name}'")
+    conf_list.sort(key=lambda x: (x[1].date, int(x[1].live_id)))
+    return conf_list
+
+
+def reconstruct_filename(attrs):
+    filename = f'{attrs.date}-{attrs.live_id}-{attrs.stage}'
+    if attrs.perfnum is not None:
+        filename += f'-{attrs.perfnum}'
+    filename += '.yml'
+    return filename

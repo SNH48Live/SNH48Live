f20886648a5b7f17e8ec16a08d6e1fa44be7a1e1
SNH48 Live <snh48live@gmail.com>
2017-10-08 18:36:28 -0400

    bin/common.py: implement mail_on_exception
    Enables email notifications when scripts abort with uncaught
    exceptions. The first scripts to adopt this are download and upload, the
    ones that are crucial to the success of the channel, and which are run
    on remote servers largely unmonitored.

--- bin/common.py
+++ bin/common.py
@@ -1,5 +1,8 @@
 import logging
 import pathlib
+import sys
+import traceback
+import __main__
 
 import appdirs
 
@@ -35,6 +38,34 @@ _handler.setFormatter(logging.Formatter(
 logger = logging.getLogger('snh48live')
 logger.addHandler(_handler)
 logger.setLevel(logging.DEBUG)
+
+
+def mail_on_exception():
+    import config
+    import mail
+
+    def excepthook(etype, value, tb):
+        try:
+            script_name = pathlib.Path(__main__.__file__).name
+        except AttributeError:
+            script_name = 'script'
+
+        if etype.__module__ == 'builtins':
+            ename = etype.__name__
+        else:
+            ename = etype.__module__ + '.' + etype.__name__
+
+        subject = f'[SNH48Live] {script_name} failed with {ename}'
+        emsg = ''.join(traceback.format_exception(etype, value, tb))
+
+        if config.main.notifications:
+            mail.send_mail(subject, emsg, config.main.mailto)
+
+        sys.stderr.write(emsg)
+        sys.exit(1)
+
+    mail.init_gmail_client()
+    sys.excepthook = excepthook
 
 
 def change_logging_format(fmt=_default_fmt, datefmt=_default_datefmt):

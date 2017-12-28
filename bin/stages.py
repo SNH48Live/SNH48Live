abcde51f8894f6e2367c8dd1032c88951f7a892c
SNH48 Live <snh48live@gmail.com>
2017-12-28 04:57:36 -0500

    Handle Team HⅡ 十八个闪耀瞬间 performances

--- bin/stages.py
+++ bin/stages.py
@@ -11,11 +11,15 @@ def title2stage(title):
     for stage in STAGES:
         if stage in title:
             return stage
+    if '十八个闪耀瞬间' in title:
+        return '十八个闪耀瞬间'
     return None
 
 
 def stage2team(stage):
-    if stage is None:
+    if stage == '十八个闪耀瞬间':
+        return 'h2'
+    elif stage is None:
         return 'joint'
     elif stage in STAGES:
         return STAGES[stage]['team']
